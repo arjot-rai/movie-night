91ce955c44b9f209e0cb96f5004f0279cc037e6d
Scot Yellowlees <scot.yellowlees@gmail.com>
2020-11-12 12:11:14 -0600

    made ApiQuery format and return Movie objects

--- src/ApiQuery.java
+++ src/ApiQuery.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -29,22 +30,96 @@ public class ApiQuery {
   /**
    * * Search the Open Movie Database API for movies that match a given search term
    *
-   * @param titleSearch the user-inputted string to search for matches
+   * @param titleSearch String: the user-inputted string to search for matches
+   * @param pages int: the number of pages of results to return - each page contains 3 results
    * @throws IOException if fails to open connection or read filestream
    * @throws ParseException if encounters malformed JSON
    */
-  public void searchMovie(String titleSearch) throws IOException, ParseException {
+  public ArrayList<Object> searchMovies(String titleSearch, int pages)
+      throws IOException, ParseException {
     titleSearch = formatStringForURL(titleSearch);
-    URL url = new URL(omdbUrl + omdbApiKey + "&s=" + titleSearch + "&type=movie");
+
+    ArrayList<Object> results = new ArrayList<>();
+
+    for (int i = 1; i <= pages; i++) {
+      URL url = new URL(omdbUrl + omdbApiKey + "&s=" + titleSearch + "&type=movie&page=" + i);
+      // check if the URL works
+      if (isResponseCode200(url)) {
+        JSONObject json = convertStringToJSON(readTextFromURL(url));
+
+        // search returns an array, add all of them
+        results.addAll((JSONArray) json.get("Search"));
+
+      } else {
+        System.out.println("error");
+      }
+    }
+    return results;
+    }
+
+  /**
+   * retrieve information on a specific movie
+   * @param titleInput String: the title of the movie to retrieve
+   * @return a Movie object containing all the relevant information about the movie
+   * @throws IOException if fails to open connection or read filestream
+   * @throws ParseException if encounters malformed JSON
+   */
+  public Movie getMovie(String titleInput) throws IOException, ParseException {
+    titleInput = formatStringForURL(titleInput);
+    //&t for title, &type=movie to only return movies
+    URL url = new URL(omdbUrl + omdbApiKey + "&t=" + titleInput + "&type=movie");
+
+    //response code 200 means the URL is valid
     if (isResponseCode200(url)) {
       JSONObject json = convertStringToJSON(readTextFromURL(url));
-
-      for (int i = 0; i < json.size(); i++) {
-        System.out.println(((JSONArray) json.get("Search")).get(i));
-      }
-    } else {
-      System.out.println("error");
+      return formatMovie(json);
     }
+    else return null;
+  }
+
+  /**
+   * retrieve information on a specific movie
+   * @param imdbID int: the IMDB ID of the movie to retrieve
+   * @return a Movie object containing all the relevant information about the movie
+   * @throws IOException if fails to open connection or read filestream
+   * @throws ParseException if encounters malformed JSON
+   */
+  public Movie getMovie(int imdbID) throws IOException, ParseException {
+    String id;
+    //some 8 digit IDs exist, but all other IDs need to be padded to 7 digits
+    if(imdbID < 10000000){
+      id = String.format("%07d", imdbID);
+    }
+    else{
+      id = ((Integer) imdbID).toString();
+    }
+
+    //&i for ID, &type=movie to only return movies
+    URL url = new URL(omdbUrl + omdbApiKey + "&i=tt" + id + "&type=movie");
+
+    //response code 200 means the URL is valid
+    if (isResponseCode200(url)) {
+      JSONObject json = convertStringToJSON(readTextFromURL(url));
+      return formatMovie(json);
+    }
+    else return null;
+  }
+
+  private Movie formatMovie(JSONObject json){
+    String title = (String) json.get("Title");
+    int id = Integer.parseInt(((String) json.get("imdbID")).replaceAll("t", ""));
+    String director = (String) json.get("Director");
+    String releaseDate = (String) json.get("Released");
+    String rating = (String) json.get("Rated");
+    String genre = (String) json.get("Genre");
+
+    Movie movie = new Movie(title, id, director, releaseDate, rating, genre);
+
+    String[] actors = ((String) json.get("Actors")).split(",");
+    for (String actor : actors) {
+      movie.addMovieActor(actor);
+    }
+    return movie;
   }
 
   private boolean isResponseCode200(URL url) throws IOException {
@@ -76,8 +151,6 @@ public class ApiQuery {
   }
 
   public static void main(String[] args) throws IOException, ParseException {
-    ApiQuery query = new ApiQuery();
-    query.searchMovie("blade runner");
   }
 
 
