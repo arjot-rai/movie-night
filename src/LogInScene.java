ebf199349c849d1e15818ea2deda8b395c5234a4
Josh Hall <joshall986@gmail.com>
2020-11-23 13:48:49 -0600

    Created new UI elements
    New UI elements for Creating an Account or Logging in added. Started working on UI for the MainScene
    -Josh Hall jah073

---
+++ src/LogInScene.java
@@ -0,0 +1,36 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LogInScene {
+    @FXML
+    private Button back_button;
+
+    @FXML
+    private Button enter_button;
+
+    public void pressedBack(ActionEvent event) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("OpeningScene.fxml"));
+
+        Scene scene = new Scene(pane);
+
+        Stage window = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+
+    public void pressedEnter(ActionEvent event) throws  IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+
+        Scene scene = new Scene(pane);
+        Stage window = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        window.setScene(scene);
+        window.show();
+    }
+}
