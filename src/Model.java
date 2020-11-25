7c5a2e60bfce6f66ca0a292a083376ae3b887304
Heronoxgames <aplejax4snax@hotmail.com>
2020-11-24 21:42:34 -0600

    Added the stage to the model

--- src/Model.java
+++ src/Model.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+import javafx.stage.Stage;
 
 public class Model {
 
   public User user;
   public ApiQuery apiQuery;
   private ArrayList<SceneModelListener> subscribers;
+  public Stage stage;
 
   public Model() {
     apiQuery = new ApiQuery();
     user = new User();
     subscribers = new ArrayList<>();
+    stage = new Stage();
   }
 
   public void addSubscribers(SceneModelListener sub) { subscribers.add(sub); }
