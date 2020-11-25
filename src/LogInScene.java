0ae04a8256220c916f5dbe727c930d2764c55628
Heronoxgames <aplejax4snax@hotmail.com>
2020-11-24 21:43:17 -0600

    Set up LogInScene to use new constructor method

--- src/LogInScene.java
+++ src/LogInScene.java
@@ -8,12 +8,35 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class LogInScene {
+public class LogInScene implements SceneModelListener {
+    private Model model;
+
     @FXML
     private Button back_button;
 
     @FXML
     private Button enter_button;
+
+    /*
+       * LogInScene takes in the model, calls the loader, sets the controller and sets the scene
+       * To change scenes to this scene, simply call the constructor for this scene
+
+     */
+    public LogInScene(Model newModel) {
+        model = newModel;
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("LogInScene.fxml"));
+
+            loader.setController(this);
+
+            model.stage.setScene(new Scene(loader.load()));
+
+            model.stage.setTitle("MovieNight - LogInScene");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void pressedBack(ActionEvent event) throws IOException {
         Parent pane = FXMLLoader.load(getClass().getResource("OpeningScene.fxml"));
@@ -32,5 +55,10 @@ public class LogInScene {
         Stage window = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
         window.setScene(scene);
         window.show();
+    }
+
+    @Override
+    public void modelChanged() {
+
     }
 }
