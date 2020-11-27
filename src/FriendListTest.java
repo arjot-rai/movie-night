f23ec39f1c4ab452c5d19441bb46dc0c14acc3df
say106 <say016@usask.ca>
2020-11-26 19:09:59 -0600

    modified tests to work with new FriendList and User

--- src/FriendListTest.java
+++ src/FriendListTest.java
@@ -7,20 +7,20 @@ class FriendListTest {
   @Test
   void testAddingAndRemovingFriends() {
     FriendList friendList = User.getFriendList();
-    friendList.addFriend(1);
-    friendList.addFriend(2);
-    friendList.addInvitation(3);
-    friendList.acceptInvite(3);
+    friendList.addFriend("1");
+    friendList.addFriend("2");
+    friendList.addInvitation("3");
+    friendList.acceptInvite("3");
     Assertions.assertAll(
         "Adding friends",
         () -> assertEquals("[1, 2, 3]", friendList.confirmedFriends.toString()),
         () -> assertEquals("[]", friendList.friendInvites.toString()));
 
-    friendList.removeFriend(2);
+    friendList.removeFriend("2");
     Assertions.assertEquals("[1, 3]", friendList.confirmedFriends.toString());
 
-    friendList.addInvitation(4);
-    friendList.rejectInvite(4);
+    friendList.addInvitation("4");
+    friendList.rejectInvite("4");
     Assertions.assertAll(
         "Rejecting friends",
         () -> assertEquals("[1, 3]", friendList.confirmedFriends.toString()),
