a4c413dbdc18a5ca5be07583cfb8583a5bbc78d4
Heronoxgames <aplejax4snax@hotmail.com>
2020-11-28 02:14:18 -0600

    Added movie details to MovieScene

--- src/MovieScene.java
+++ src/MovieScene.java
@@ -1,15 +1,45 @@
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class MovieScene {
-  //@FMXL
-  private Button back_button;
+  private Model model;
+  private Movie movie;
+  @FXML private TextField movie_name_textfield, movie_desc, release_date, director, streaming_services;
 
-  public void pressedBack(ActionEvent event) throws IOException {
-    //Parent pane = FXMLLoader.load(getClass().getResource(""))
+  public MovieScene(Model newModel, Movie newMovie) {
+    model = newModel;
+    movie = newMovie;
+    try {
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("MovieScene.fxml"));
+
+      loader.setController(this);
+
+      setUpMovieDetails();
+
+      model.stage.setScene(new Scene(loader.load()));
+
+      model.stage.setTitle("MovieNight - MovieScene");
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void backButtonPressed(ActionEvent event) {
+
+  }
+
+  private void setUpMovieDetails() {
+    movie_name_textfield.setText(movie.getMovieName());
+    movie_desc.setText(movie.getMoviePlot());
+    director.setText(movie.getMovieDirector());
+    release_date.setText(movie.getMovieReleaseDate());
+    //streaming_services.setText(movie.getMovieStreamingSite()); // NYI
+
   }
 
 }
