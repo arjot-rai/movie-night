9a775b189c73467ecf254a7d575f27068b819bf4
Arjot Singh Rai <arr455@usask.ca>
2020-12-03 20:34:54 -0600

    update MovieList class to refer to movies by name instead of id
    This is done in order to increase compatibility with the server, which refers to movies by their names

--- src/MovieList.java
+++ src/MovieList.java
@@ -4,52 +4,38 @@ import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class MovieList {
-  public Dictionary<Integer, Float> movieRatings; // dictionary to map movie to its rating
+  public Dictionary<String, String> movieRatings; // dictionary to map movie to its rating
 
-  public ArrayList<Integer> movies;
+  public ArrayList<String> movieNames;
 
-  public MovieList(){
-    movies = new ArrayList<>();
+  public MovieList() {
+    movieNames = new ArrayList<>();
     movieRatings = new Hashtable<>();
   }
 
-  public void addMovie(int movieID){
-    this.movies.add(movieID);
+  public void addMovie(String movieName) {
+    this.movieNames.add(movieName);
   }
 
-  public boolean giveRating(int movieID, float rating){
+  public boolean giveRating(String movieName, String rating) {
     // rating can only be given if the movie is in user's list
-    if(this.movies.contains(movieID)){
-      this.movieRatings.put(movieID, rating);
+    if (this.movieNames.contains(movieName)) {
+      this.movieRatings.put(movieName, rating);
       return true;
     }
     return false;
   }
 
-  public MovieList compareMovies(MovieList other){
+  public MovieList compareMovies(MovieList other) {
     MovieList commonMovies = new MovieList();
-    for (Integer thisMovieID: this.movies) {
-      for (Integer otherMovieID : other.movies) {
-        if (thisMovieID.equals(otherMovieID)) {
-          commonMovies.addMovie(thisMovieID);
+    for (String thisMovieName : this.movieNames) {
+      for (String otherMovieName : other.movieNames) {
+        if (thisMovieName.equals(otherMovieName)) {
+          commonMovies.addMovie(thisMovieName);
           break;
         }
       }
     }
     return commonMovies;
   }
-
-  public static void main(String[] args){
-    //testing MovieList class
-    MovieList movieList = new MovieList();
-
-    movieList.addMovie(1);
-    if(movieList.giveRating(1, 4.3f)){
-      System.out.println(movieList.movieRatings.get(1));
-    } else{
-      System.out.println("rating should not have been added");
-    }
-
-  }
-
 }
