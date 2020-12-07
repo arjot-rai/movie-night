4dd4f87f3e12877ae60f0bba0be8cf4aa9fb3c26
Heronoxgames <aplejax4snax@hotmail.com>
2020-12-06 22:16:53 -0600

    Resizes gridpane for movies to vote on, enlarged movie images, added list of buttons to track them.  Added vote functionality.

--- src/EventScene.java
+++ src/EventScene.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -22,7 +26,7 @@ public class EventScene {
   private TextArea attendees_text, date_text, location_text, streaming_text;
 
   @FXML
-  private Button yes_attending, no_attending, back_button;
+  private Button yes_attending, no_attending, back_button, add_attendee_button;
 
   @FXML
   private ImageView profile_picture;
@@ -33,10 +37,16 @@ public class EventScene {
   @FXML
   private GridPane vote_gridpane;
 
+  @FXML
+  private AnchorPane vote_anchorpane, main_event_anchorpane;
+
+  private ArrayList<Button> voteButtonList;
+
   public EventScene(Model newModel, Event newEvent) {
     model = newModel;
     thisEvent = newEvent;
     apiQuery = new ApiQuery();
+    voteButtonList = new ArrayList<>();
     try {
       FXMLLoader loader = new FXMLLoader(getClass().getResource("EventScene.fxml"));
       loader.setController(this);
@@ -74,6 +84,15 @@ public class EventScene {
     streaming_text.setText(serviceText.toString());
     streaming_text.setEditable(false);
     profile_picture.setImage(ProfilePicture.getProfilePic(thisEvent.getEventOrganizer()));
+    if (!thisEvent.getEventOrganizer().equals(User.getUserName())) {
+      add_attendee_button.setDisable(true);
+    }
+  }
+
+  public void pressedInviteButton(ActionEvent e) throws IOException {
+    main_event_anchorpane.setDisable(true);
+    InviteToEventScene inviteToEventScene = new InviteToEventScene(model, thisEvent);
+    main_event_anchorpane.setDisable(false);
   }
 
 
@@ -87,7 +106,6 @@ public class EventScene {
   public void pressedNoAttending(ActionEvent e) throws IOException {
     if (User.getEventList().confirmedEvents.contains(thisEvent)) {
       Server.removeEventInvite(User.getUserName(), thisEvent.getEventID());
-      MainScene mainScene = new MainScene(model);
     }
   }
 
@@ -99,13 +117,20 @@ public class EventScene {
    * Set up the movies in the grid
    */
   public void displayMovies() {
-    System.out.println(thisEvent.getEventMovies());
+    //System.out.println(Server.getEventAttendees(thisEvent.getEventID()).get(User.getUserName()).get());
+    //System.out.println(Server.getUsersVotedMovie(User.getUserName(), thisEvent.getEventID()));
+    String userVote = Server.getUsersVotedMovie(User.getUserName(), thisEvent.getEventID());
+
+    HashMap<String, Map<String, Object>> movieVotes = Server.getEventMovies(thisEvent.getEventID());
+    ArrayList<String> movieTitlesList = new ArrayList<>();
+    movieVotes.forEach((movieTitle, votes) -> {movieTitlesList.add(movieTitle);});
+
     int moviesShown = 0;
-    for (int rowIndex = 0; rowIndex < (thisEvent.getEventMovies().size() / 2) + 1; rowIndex++) {
+    for (int rowIndex = 0; rowIndex < (movieVotes.size() / 2) + 1; rowIndex++) {
       for (int columnIndex = 0; columnIndex < 2; columnIndex++) {
-        if (moviesShown < thisEvent.getEventMovies().size()) {
-          Movie movie = apiQuery.getMovie(thisEvent.getEventMovies().get(rowIndex * 2 + columnIndex));
-          VBox vBox = createVotingBox(movie);
+        if (moviesShown < movieVotes.size()) {
+          Movie movie = apiQuery.getMovie(movieTitlesList.get(rowIndex * 2 + columnIndex));
+          VBox vBox = createVotingBox(movie, userVote);
           vote_gridpane.add(vBox, columnIndex, rowIndex);
           //TODO Create a button to bring you to movie page
 
@@ -115,6 +140,8 @@ public class EventScene {
 
       }
     }
+    vote_anchorpane.setMinSize(vote_anchorpane.getPrefWidth(), ((1 + (float) movieVotes.size()) / 2 * 155));
+    vote_anchorpane.setMaxSize(vote_anchorpane.getPrefWidth(), ((1 + (float) movieVotes.size()) / 2 * 155));
   }
 
   /**
@@ -122,23 +149,26 @@ public class EventScene {
    * @param movie The movie to create the VBox for
    * @return returns the VBox with the movie picture, vote button and vote total
    */
-  private VBox createVotingBox(Movie movie) {
-    Image image = new Image(movie.getMoviePosterUrl(), 72, 112, false, false);
+  private VBox createVotingBox(Movie movie, String userVote) {
+    Image image = new Image(movie.getMoviePosterUrl(), 92, 142, false, false);
     ImageView movieImage = new ImageView(image);
 
     VBox vBox = new VBox();
     vBox.getChildren().add(movieImage);
 
-    // Check if username has voted on this movie to set the button status
-    Button voteButton = new Button();
-    if (!thisEvent.getMovieVotingRecord().get(movie.getMovieName()).contains(User.getUserName())) {
-      voteButton.setText("Vote");
-      voteButton.setOnAction(e -> { pressedVoteButton(voteButton, movie); });
+
+    Button voteButton = new Button("Vote");
+    voteButtonList.add(voteButton);
+    if (userVote.equals("no")) {
+      voteButton.setDisable(true);
     } else {
-      voteButton.setText("Voted");
-      voteButton.setOnAction(e -> { pressedVotedButton(voteButton, movie); });
+      voteButton.setOnAction(e -> { pressedVoteButton(movie);});
     }
-    Label voteLabel = new Label("Votes: " + thisEvent.getMovieVoteTotals().toString());
+    Label voteLabel = new Label();
+    String votes = Server.getEventMovies(thisEvent.getEventID()).get(movie.getMovieName()).get("vote").toString();
+    voteLabel.setText("Votes: " + votes);
+
+
     HBox hBox = new HBox();
     hBox.getChildren().addAll(voteButton, voteLabel);
 
@@ -147,19 +177,12 @@ public class EventScene {
     return vBox;
   }
 
-  private void pressedVoteButton(Button button, Movie movie) {
-    button.setText("Voted");
-    button.setOnAction(e -> { pressedVotedButton(button, movie); });
-
-    Server.addMovieVote(movie.getMovieName(), thisEvent.getEventID());
-  }
-
-  private void pressedVotedButton(Button button, Movie movie) {
-    button.setText("Vote");
-    button.setOnAction(e -> { pressedVotedButton(button, movie); });
-
-    thisEvent.removeVoter(movie.getMovieName(), User.getUserName());
-    Server.reduceMovieVote(movie.getMovieName(), thisEvent.getEventID());
+  private void pressedVoteButton(Movie movie) {
+    Server.userVoted(User.getUserName(), thisEvent.getEventID(), movie.getMovieName());
+    //Update labels/buttons
+    for (Button button : voteButtonList) {
+      button.setDisable(true);
+    }
   }
 
 
